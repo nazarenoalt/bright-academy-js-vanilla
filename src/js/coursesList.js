ccd3f4cfe3cfe249bc0b79f9486f98e63454bb9f
Nazareno Altamiranda <nazalt256@gmail.com>
2021-02-23 12:10:03 -0300

    fixing courses-icon wrapping

--- src/js/coursesList.js
+++ src/js/coursesList.js
@@ -1,34 +1,41 @@
 const coursesContainer = document.getElementById('courses-list');
 const HTMLCollectionCourses = document.getElementsByClassName('course-container');
 const arrayCourses = []; //initializing array
-
+const mediaQuery = window.matchMedia( 'only screen (max-width: 500px)' );
 //transfer all HTMLcollectionCourses content to arrayCourses
 for(let i = 0; i < HTMLCollectionCourses.length; i++) {
     arrayCourses.unshift(HTMLCollectionCourses[i])
 }
 
 createDivCourses(3, 5);
+if(mediaQuery.matches) {
 
+}
 function createDivCourses(firstDiv, secondDiv) {
     let counter = 0;
-    
     
     for(i = 1; i < arrayCourses.length; i++) {
         counter++
 
         if(counter === firstDiv ) {
 
-            let firstRow = document.createElement('div'); 
-            firstRow.classList.add('course-row') 
-            firstRow.append(arrayCourses[i-2], arrayCourses[i-1], arrayCourses[i]); 
-            coursesContainer.insertAdjacentElement("afterbegin", firstRow);
+            let row = document.createElement('div'); 
+            row.classList.add('course-row') 
+            
+            row.append(arrayCourses[i-2], arrayCourses[i-1], arrayCourses[i]); 
+            coursesContainer.insertAdjacentElement("afterbegin", row);
+
+            if(mediaQuery.matches) {
+                row.classList.add('wrapped') 
+                arrayCourses[i-2].classList.add('order-1');
+            }
 
         } else if (counter === secondDiv) {
 
-            let firstRow = document.createElement('div');
-            firstRow.classList.add('course-row')
-            firstRow.append(arrayCourses[i-1], arrayCourses[i]);
-            coursesContainer.insertAdjacentElement("afterbegin", firstRow);
+            let row = document.createElement('div');
+            row.classList.add('course-row')
+            row.append(arrayCourses[i-1], arrayCourses[i]);
+            coursesContainer.insertAdjacentElement("afterbegin", row);
             
             counter = 0;
 
--- src/js/login.js
+++ src/js/login.js
@@ -1,20 +1,17 @@
 const loginForm = document.querySelector('#login-form');
-const loginButton = document.querySelector('#login-button')
 const login_URL = 'http://gonza56d.pythonanywhere.com/api/users/login/';
 let xmlHttp = new XMLHttpRequest();
 
 loginForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('hola');
-
     formData = new FormData(loginForm);
-    
     xmlHttp.open("POST", login_URL, true); 
-    xmlHttp.setRequestHeader("Access-Control-Allow-Headers", "Accept");
     xmlHttp.send(formData);
     xmlHttp.onreadystatechange = function() {
-            if(xmlHttp.readyState == 4) {
-                alert(xmlHttp.responseText);
-            }
+        if(xmlHttp.readyState == 4) {
+            alert(xmlHttp.responseText);
         }
+    }
 })
+
+//Revisar funcion require
