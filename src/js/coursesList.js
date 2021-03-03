dd1c2bc7bc25e3a2df413647b0af0ddfa8964353
Nazareno Altamiranda <nazalt256@gmail.com>
2021-03-03 19:56:14 -0300

    changin js script of courses-list by one more efficient

--- src/js/coursesList.js
+++ src/js/coursesList.js
@@ -1,46 +1,30 @@
-const coursesContainer = document.getElementById('courses-list');
-const HTMLCollectionCourses = document.getElementsByClassName('course-container');
-const arrayCourses = []; //initializing array
-const mediaQuery = window.matchMedia( 'only screen and (max-width: 500px)' );
-//transfer all HTMLcollectionCourses content to arrayCourses
-for(let i = 0; i < HTMLCollectionCourses.length; i++) {
-    arrayCourses.unshift(HTMLCollectionCourses[i])
+const newContainer = document.getElementById('new-container');
+
+let divs = document.querySelectorAll('.course-container');
+let nodos = [];
+let counter = 0;
+
+for (let i = 0; i < divs.length; i++) {
+    counter++;
+
+    if(counter === 2) {
+
+        let nodo = document.createElement('div');
+        nodo.classList.add('wrapped') 
+
+        nodo.append(divs[i-1], divs[i])
+        nodos.push(nodo);
+        
+    } else if(counter === 5) {
+
+        let nodo = document.createElement('div');
+        nodo.classList.add('wrapped') 
+
+        nodo.append(divs[i-2], divs[i-1], divs[i])
+        nodos.push(nodo)
+        counter = 0;
+
+   }
 }
 
-createDivCourses(3, 5);
-if(mediaQuery.matches) {
-
-}
-function createDivCourses(firstDiv, secondDiv) {
-    let counter = 0;
-    
-    for(i = 1; i < arrayCourses.length; i++) {
-        counter++
-
-        if(counter === firstDiv ) {
-
-            let row = document.createElement('div'); 
-            row.classList.add('course-row') 
-            
-            row.append(arrayCourses[i-2], arrayCourses[i-1], arrayCourses[i]); 
-            coursesContainer.insertAdjacentElement("afterbegin", row);
-
-            if(mediaQuery.matches) {
-                row.classList.add('wrapped') 
-                arrayCourses[i-2].classList.add('order-1');
-            }
-
-        } else if (counter === secondDiv) {
-
-            let row = document.createElement('div');
-            row.classList.add('course-row')
-            row.append(arrayCourses[i-1], arrayCourses[i]);
-            coursesContainer.insertAdjacentElement("afterbegin", row);
-            
-            counter = 0;
-
-        }
-    
-    }
-   
-}
+newContainer.append(...nodos);
