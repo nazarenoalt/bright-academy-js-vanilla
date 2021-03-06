2577e6546adca5951b8cbfb84deb87c024aba01a
Nazareno Altamiranda <nazalt256@gmail.com>
2021-03-06 00:28:03 -0300

    making text editor

--- src/js/post-creation.js
+++ src/js/post-creation.js
@@ -1,12 +1,25 @@
-var options = {
-    debug: 'info',
-    modules: {
-            toolbar: {
-                container: '#toolbar',  // Selector for toolbar container
-        placeholder: 'Create wonderful classes for your students =)',
-        theme: 'snow'
-        }
-    }
+let toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote'],
+  
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    ['image', 'video'],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
+
+  let options = {
+    theme: 'snow',
+     modules: {
+        toolbar: toolbarOptions
+     },
+   
 }
 
 let editor = new Quill('#editor-container', options);
