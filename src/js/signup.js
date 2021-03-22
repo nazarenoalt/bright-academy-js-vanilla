db9d903206933844baa3efb2d57c8cdbd9159ac5
gonza <gonza56d@gmail.com>
2021-03-22 18:49:14 -0300

    making fetch-post.js

--- src/js/signup.js
+++ src/js/signup.js
@@ -1,6 +1,5 @@
 const signupForm = document.querySelector('#signup-form');
 const signup_URL = 'http://gonza56d.pythonanywhere.com/api/users/signup/';
-let xmlHttp = new XMLHttpRequest();
 
 signupForm.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -16,15 +15,3 @@ signupForm.addEventListener('submit', (event) => {
         .then(data => console.log(data))
         .catch(err => console.error(err))
 })
-
-/* signupForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    formData = new FormData(signupForm);
-    xmlHttp.open("POST", signup_URL, true); 
-    xmlHttp.send(formData);
-    xmlHttp.onreadystatechange = function() {
-        if(xmlHttp.readyState == 4) {
-            alert(xmlHttp.responseText);
-        }
-    }
-}) */
