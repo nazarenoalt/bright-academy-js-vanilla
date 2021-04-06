fe6895da41a516ae61cd386d8d520c60c5427e24
gonza <gonza56d@gmail.com>
2021-04-06 15:19:51 -0300

    fixed / burger-menu.svg 404 not found

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,21 +1,23 @@
-    const sidebarElement = document.getElementById('sidebar');
-    const mediaQuery = window.matchMedia('screen and (min-width: 1024px)');
+const sidebarElement = document.getElementById('sidebar');
+const mediaQuery = window.matchMedia('screen and (min-width: 1024px)');
 
-    if(mediaQuery.matches) {
-        sidebar.classList.add('sidebar-desktop');
+if (mediaQuery.matches) {
+    sidebar.classList.add('sidebar-desktop');
+} else {
+    sidebar.style.display = 'none'
+}
+
+window.addEventListener('onload', () => {
+    const menu = document.querySelector('#menu')
+    menu.onclick(openCloseSidebar)
+    console.log(menu)
+})
+function openCloseSidebar() {
+    console.log('HOLAAAAs')
+    if (sidebar.style.display === 'none') {
+        sidebar.style.display = 'block'
     } else {
         sidebar.style.display = 'none'
     }
+}
 
-    window.addEventListener('load', () => {
-        const menu = document.querySelector('#menu')
-        menu.onclick = openCloseSidebar;
-    })
-    
-    function openCloseSidebar() {
-        if(sidebar.style.display === 'none') {
-            sidebar.style.display = 'block'
-        } else {
-            sidebar.style.display = 'none'
-        }
-    }
