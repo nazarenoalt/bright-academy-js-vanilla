95e984995253b153459ac8bbc61e297440663996
gonza <gonza56d@gmail.com>
2021-04-06 13:56:41 -0300

    authentication problem solved

---
+++ problems.md
@@ -0,0 +1,3 @@
+[SOLVED] Auth function doesn't work \
+[PENDING] Login has to redirect to index \
+[PENDING] Solve grid of guest.html \
--- src/js/auth.js
+++ src/js/auth.js
@@ -1,27 +1,25 @@
 export function userAuthentication(fun) {
-    let cookies = document.cookie.split(';');
-    let authCookie = cookies.map(cookie => cookie.split('=')).filter(cookie => cookie[0] === "access_token");
 
-    let AUTH_TOKEN;
-    if (authCookie.length !== 0) {
-        const AUTH_TOKEN = authCookie[0][1];
-    } else {
-        console.log('hola')
-    }
     const API_URL = 'http://brightacademy.pythonanywhere.com/api/utils/validate_token/';
+    const AUTH_COOKIE = document.cookie //return string token
+        .split(';')
+        .map(cok => cok.split('='))
+        .filter(cookie => cookie[0] === "access_token");
 
-    fetch(API_URL, {
+    const AUTH_TOKEN = () => AUTH_COOKIE.length !== 0 ? AUTH_COOKIE[0][1] : '';
+
+    fetch(API_URL, { //fetch api and return if AUTH_TOKEN = token Api
         method: 'POST',
         headers: {
-            Authorization: `Token ${AUTH_TOKEN}`
+            Authorization: `Token ${AUTH_TOKEN()}`
         }
     })
         .then(response => {
             if (response.ok) {
-                return 'todo okay!'
                 fun();
             } else {
                 window.location = 'http://localhost:63342/bright-academy/guest.html'
             }
         })
+
 }
