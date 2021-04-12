91e4575832f7dedd00153141ba7dd0b771a54fac
gonza <gonza56d@gmail.com>
2021-04-12 16:42:57 -0300

    fixing/fetch-post-doesnt-work

--- src/js/fetch-post.js
+++ src/js/fetch-post.js
@@ -1,20 +1,19 @@
+import { API_URL} from "./components/constants.js";
 
 const postFormulary = document.querySelector('#post_container');
-const URL = 'http://gonza56d.pythonanywhere.com/api/posts/:id/';
 const post_id = '1';
 const AUTH_TOKEN = '2587b85d61320f51e6eecd6b4ee0e88b6bf390a0'
+const API_POST_URL = `${API_URL}posts/:id/`
 
 //post data
-    fetch(URL.replace(':id', post_id), {
+    fetch(API_POST_URL.replace(':id', post_id), {
         headers: {
             'Authorization': `Token ${AUTH_TOKEN}`,
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'http://localhost:63342',
         }
     })
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             post_title.innerHTML = data.title;
             post_content.innerHTML = data.content;
             let date = new Date(data.created);
