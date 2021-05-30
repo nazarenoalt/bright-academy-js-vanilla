8a332481ded5073f8cb1c2137b73d4e130d4db1f
Nazareno Altamiranda <nazalt256@gmail.com>
2021-05-30 15:37:59 -0300

    fixing edit-profile fetching

--- src/js/user/my-account/edit-profile.js
+++ src/js/user/my-account/edit-profile.js
@@ -6,21 +6,23 @@ const username = getCookies().username;
 const URL = `${API_URL}profiles/${username}/`;
 
 form.addEventListener('submit', (e) => {
+  
   e.preventDefault();
   const formData = new FormData(form);
+  
   fetch(URL, {
     method: 'patch',
     body: formData,
     headers: {
       Authorization: `Token ${AUTH_TOKEN}`,
+      'Content-Type': 'application/json'
     }
   })
-  .then((response) => {
-    debugger
+    .then((response) => {
     response.json()
     document.location = `${ORIGIN_ROOT}/my-account.html`
-  })
-  .then(data => console.log(data))
-  .catch(err => console.error(err))
+    })
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
   
 })
