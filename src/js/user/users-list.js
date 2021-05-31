9e9d856984cef6cad95a50362876ad2c33ae3134
Nazareno Altamiranda <nazalt256@gmail.com>
2021-05-31 01:02:04 -0300

    fixing things

--- src/js/user/users-list.js
+++ src/js/user/users-list.js
@@ -1,10 +1,15 @@
-import generateUsersList from '../utils/generate-users-list.js'
+import { API_URL, AUTH_TOKEN } from '../utils/constants.js';
 
-const list = generateUsersList();
+const generateUsersList = () => {
+  const URL = `${API_URL}users/`;
 
-console.log('users list')
-console.log("users list: "+ list)
-
-list.map( user => {
-  console.log(user)
-})
+  fetch(URL, {
+    headers: {
+      Authorization: `Token ${AUTH_TOKEN}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      data.map(user => console.log(user))
+    })
+}
