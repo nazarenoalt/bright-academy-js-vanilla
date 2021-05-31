9231664ff79384d80e20dc56c9632ed867b84301
Nazareno Altamiranda <nazalt256@gmail.com>
2021-05-31 01:23:42 -0300

    fixing things

--- src/js/user/users-list.js
+++ src/js/user/users-list.js
@@ -1,4 +1,5 @@
 import { API_URL, AUTH_TOKEN } from '../utils/constants.js';
+
 const generateUsersList = () => {
   const URL = `${API_URL}users/`;
   
@@ -9,9 +10,27 @@ const generateUsersList = () => {
   })
   .then(response => response.json())
   .then(data => {
-    data.map(user => console.log(user))
+    const table = document.createElement('table')
+
+    data.map(user => {
+      console.log(user)
+
+      const user_tr = document.createElement('tr');
+
+      const td_name = document.createElement('td');
+      td_name.innerText = user.username;
+      const td_role = document.createElement('td');
+      td_role.innerText = user.profile.profile_type;
+      const td_level = document.createElement('td');
+      td_level.innerText = user.profile.level;
+
+      user_tr.append(td_name, td_role, td_level)
+      table.append(user_td);
+    })
+
+    
   })
 }
 
 generateUsersList();
-console.log('users-list-v1')
+console.log('users-list-v2')
