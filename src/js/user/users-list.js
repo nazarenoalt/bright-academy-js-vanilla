de6a93c7eb714a37e3e6edd770ad0426407d91f0
Nazareno Altamiranda <nazalt256@gmail.com>
2021-05-31 00:56:20 -0300

    fixing things

--- src/js/user/users-list.js
+++ src/js/user/users-list.js
@@ -2,4 +2,9 @@ import generateUsersList from '../utils/generate-users-list.js'
 
 const list = generateUsersList();
 
+console.log('users list')
 console.log("users list: "+ list)
+
+list.map( user => {
+  console.log(user)
+})
