8ac3d231cd1b9d00a78a57ee174ed24ce614daa3
Nazareno Altamiranda <nazalt256@gmail.com>
2021-05-31 02:07:11 -0300

    fixing things

--- src/js/user/edit-role.js
+++ src/js/user/edit-role.js
@@ -1,5 +1,20 @@
 const button = document.querySelector('#edit-role');
+const container = document.querySelector('.main-section');
 
 button.addEventListener('click', () => {
-  
+  const form = document.createElement('form')
+  form.setAttribute('id', "role_form")
+  form.innerHTML = `
+    <select name="role-selector" id="role-selector">
+      <option value="S">Student</option>
+      <option value="T">Teacher</option>
+    </select>
+    <button type="submit" class="tag" id="submit_role">cambiar rol</button>
+  `
+  container.append('form')
+  const role_form = document.querySelector('#role_form');
+  role_form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('sale submit');
+  })
 })
